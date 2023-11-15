682466ca37ea5227310588d0e0fb91025adf8c45
Vaideeswaran21 <120708821+Vaideeswaran21@users.noreply.github.com>
2023-11-15 21:47:37 +0530

    Add files via upload

---
+++ Check.py
@@ -0,0 +1,27 @@
+# Libraries
+import numpy as np
+import os
+import pandas as pd
+
+# Load the labels from the .csv file
+df = pd.read_csv('C:/Users/vaide/OneDrive/Documents/IIT Hyderabad/Semester 5/Full Semester Courses/PRML\Diabetic_Retinopathy/trainLabels.csv/trainLabels.csv')
+
+df['image'] = df['image']
+temp = []
+
+directory = os.listdir("C:/Users/vaide/OneDrive/Documents/IIT Hyderabad/Semester 5/Full Semester Courses/PRML\Diabetic_Retinopathy/preprocess")
+list_len = len(directory)
+print(list_len)
+counter = 0
+
+for y in df['image']:
+    y = y + '.jpeg'
+    temp.append(y)
+
+
+for x in range(list_len):
+    if directory[x] in temp:
+        counter  = counter + 1 
+    else:
+        print(directory[x])
+print(counter)
